14f36d9663e896ba4d9aaa7beb4b0a95dd49ad91
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2026-07-14 21:34:51 +0200

    Migration to latest dependencies, min sdk = 23

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,15 +8,9 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.hilt) apply false
 
     // Nordic plugins are defined in https://github.com/NordicSemiconductor/Android-Gradle-Plugins
-    alias(libs.plugins.nordic.application) apply false
-    alias(libs.plugins.nordic.application.compose) apply false
-    alias(libs.plugins.nordic.hilt) apply false
+    alias(libs.plugins.nordic.android.application) apply false
+    alias(libs.plugins.nordic.feature.hilt.compose) apply false
 }
